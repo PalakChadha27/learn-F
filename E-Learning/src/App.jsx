fec739799bf16ae88ec50acbfb88e2f209fc91f7
Naman Tulsyan <namantulsyan976@gmail.com>
2025-03-30 20:51:55 +0530

    Mentor Dashboard

--- E-Learning/src/App.jsx
+++ E-Learning/src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import Signup from "./components/Signup";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
-  );
-};
+    <>
+      <div>
+        <Dashboard/>
+      </div>
+    
+    </>
+  )
+}
 
 export default App;
